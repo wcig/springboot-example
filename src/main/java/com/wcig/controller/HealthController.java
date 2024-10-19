782cd4a2972ba01e7d03bbf791e707f9778e899b
wcig <job.yangbo@outlook.com>
2024-10-19 12:03:59 +0800

    add: log config with logback

--- src/main/java/com/wcig/controller/HealthController.java
+++ src/main/java/com/wcig/controller/HealthController.java
@@ -1,14 +1,20 @@
 package com.wcig.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HealthController {
 
     @RequestMapping(value = "/live", method = RequestMethod.GET)
     public String live() {
+        log.error(">> live");
+        log.warn(">> live");
+        log.info(">> live");
+        log.debug(">> live");
         return "ok";
     }
 
